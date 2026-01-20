9d352696b102835b11ba69a6be50e4823163c741
CodeMonkey <abhimanyug987@gmail.com>
2026-01-20 15:25:50 +0530

    fix: CORS policy error- No slash after main URL

--- server/app.js
+++ server/app.js
@@ -14,7 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://chrono-rust.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://chrono-rust.vercel.app'],
+    credentials: true,
   }),
 )
 
